c4d64509056ef248d31c7723dd8a9cc8bb182896
Doug Sebastian <dasebast@gmail.com>
2016-11-20 12:25:38 -0600

    before testing

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,10 +1,33 @@
 const React = require('react')
-const shows = require('../public/data')
+const ShowCard = require('./ShowCard')
+const data = require('../public/data')
 
-const Search = () => (
-  <pre><code>
-    {JSON.stringify(shows, null, 4)}
-  </code></pre>
-)
+const Search = React.createClass({
+  getInitialState (props) {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>reflix</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {data.shows
+            .filter((showProp) => `${showProp.title} ${showProp.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((showProp) => (
+              <ShowCard {...showProp} key={showProp.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
